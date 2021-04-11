64a72809205a64e839a88b1b52452eda08f043b4
AlexAuragan <alexauragan@gmail.com>
2021-04-11 04:41:50 +0200

    Répartition des images dans différents folders
    J'ai mis le programme python avec, j'ai tout commenté parce que mdrrr c'est pas un programme qu'on veut faire tourner deux fois.
    Il prends 1/5 des data et les mets dans un dossier test et un autre 1/5 et les mets dans un dossier train.
    On doit donc le faire qu'une seule fois parce que si on le réexecute ça va encore prendre des photos de data

---
+++ src/separe_file.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import shutil
+
+path = "../data"
+champions = ["Chogath", "Ezreal", "Lucian", "Malzahar", "Morgana", "Poppy", "Reksai", "Senna", "Syndra", "Teemo"]
+
+def create_dir(my_folder): # Create the folders for the training dataset and the test dataset
+    directory = os.path.dirname(my_folder)
+    if not os.path.exists(my_folder):
+        os.makedirs(my_folder)
+        print("making dir")
+
+
+
+List_champions_pictures = []
+for i in range(len(champions)) :
+    List_champions_pictures.append(glob.glob(path+"/"+champions[i]+"/"+"*.bmp"))
+
+
+
+for champ in champions : 
+    create_dir(path+"/test_"+champ)
+    create_dir(path+"/train_"+champ)
+
+if len(os.listdir(path+"/train_"+"teemo")) == 0 : # Just to make sure I use the program only once
+    for i in range(len(champions)) : # Take a fifth of the picture to the test folder and a fifth to the train folder
+        for num_pic in range(len(List_champions_pictures[i])) :
+            if num_pic % 5 == 0 :
+                shutil.move(List_champions_pictures[i][num_pic], path+"/test_"+champions[i] ) 
+            if num_pic % 5 == 1 :
+                shutil.move(List_champions_pictures[i][num_pic], path+"/train_"+champions[i] ) 
