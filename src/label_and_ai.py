66d8eb564aff824c9475f8adac6c107debf28aed
AlexAuragan <alexauragan@gmail.com>
2021-04-16 18:40:16 +0200

    tout qui marche babyyyy
    92% accuracy babyyyyyyyyyy

--- src/label_and_ai.py
+++ src/label_and_ai.py
@@ -1,84 +1,13 @@
 # %%
-import os
-import shutil
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-import PIL
-from PIL import Image
-import os, sys
-from scipy.io import loadmat
-
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.constraints import maxnorm
-from keras.utils import np_utils
-
-# %%
-
-def load_data(data_path, classes, dataset='train', image_size=64):
-
-    num_images = 0
-    for i in range(len(classes)):
-        dirs = sorted(os.listdir(data_path + dataset + '/' + classes[i]))
-        num_images += len(dirs)
-                                
-    x = np.zeros((num_images, image_size, image_size, 3))
-    y = np.zeros((num_images, 1))
-    
-    current_index = 0
-    
-    # Parcours des différents répertoires pour collecter les images
-    for idx_class in range(len(classes)):
-        dirs = sorted(os.listdir(data_path + dataset + '/' + classes[idx_class]))
-        num_images += len(dirs)
-    
-        # Chargement des images, 
-        for idx_img in range(len(dirs)):
-            item = dirs[idx_img]
-            if os.path.isfile(data_path + dataset + '/' + classes[idx_class] + '/' + item):
-                # Ouverture de l'image
-                img = Image.open(data_path + dataset + '/' + classes[idx_class] + '/' + item)
-                # Redimensionnement de l'image et écriture dans la variable de retour x 
-                img = img.resize((image_size,image_size))
-                x[current_index] = np.asarray(img)
-                # Écriture du label associé dans la variable de retour y
-                y[current_index] = idx_class
-                current_index += 1
-                
-    return x, y
-
-    
 
 
 # %%
 
-path = "../data/"
-labels = ["Chogath", "Ezreal", "Lucian", "Malzahar", "Morgana", "Poppy", "Reksai", "Senna", "Syndra", "Teemo"]
 
-#x_train, y_train = load_data(path, labels, dataset='train', image_size=600)
-#print(x_train.shape, y_train.shape)
 
-#x_val, y_val = load_data(path, labels, dataset='validation', image_size=600)
-#print(x_val.shape, y_val.shape)
+# %%
 
-#x_test, y_test = load_data(path, labels, dataset='test', image_size=600)
-#print(x_test.shape, y_test.shape)
 
-#class_num = y_test.shape[1]
-
-#plt.figure(figsize=(12, 12))
-#shuffle_indices = np.random.permutation(9)
-#for i in range(0, 9):
-#    plt.subplot(3, 3, i+1)
-#    image = x_train[shuffle_indices[i]]
-#    plt.title(labels[int(y_train[shuffle_indices[i]])])
-#    plt.imshow(image/255)
-
-#plt.tight_layout()
-#plt.show()
 
 # %% 
 from keras.preprocessing.image import ImageDataGenerator
